9e57aa174c1b1cd85a440850ccfbf43f0f2c406f
Roxxiebstar <kathebuitragor8@gmail.com>
2024-04-11 16:04:47 -0500

    Creación de una clase llamada questionario al igual que su xml planear_cita_questionario1.xml, para al dar click en crear tu primera cita, lo dirija allí.

---
+++ DateVenture/app/src/main/java/com/example/dateventure/questionario.kt
@@ -0,0 +1,15 @@
+package com.example.dateventure
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// creacion de clase questionario para creacion de citas
+class questionario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.planear_cita_questionario1)
+
+        // Aquí podemos empezar la logica:)
+    }
+}
