bed19a4d2e424cf5eba01e866ff2dd1694db2cd8
Sergio Galindo <141683974+Im-Scandal@users.noreply.github.com>
2024-04-24 15:56:34 -0500

    Agregando funcionalidades a los botones

--- DateVenture/app/src/main/java/com/example/dateventure/LogIn.kt
+++ DateVenture/app/src/main/java/com/example/dateventure/LogIn.kt
@@ -1,6 +1,28 @@
 package com.example.dateventure
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class LogIn {
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.log_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LogMain)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
+        val btn: Button = findViewById(R.id.register_button)
+        btn.setOnClickListener {
+            val intent: Intent = Intent(this, SignIn:: class.java)
+            startActivity(intent)
+        }
+    }
 }
