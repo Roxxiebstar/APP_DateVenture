a66a59a0b86ca1765c8fd22296de717825353b21
Sergio Galindo <141683974+Im-Scandal@users.noreply.github.com>
2024-05-09 00:58:27 -0500

    Eliminando barra lateral

--- DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
+++ DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
@@ -16,7 +16,6 @@ import com.example.dateventure.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +31,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
         val btnPlan: Button = findViewById(R.id.btnPlaneaCita)
         btnPlan.setOnClickListener {
@@ -50,17 +42,6 @@ class MainActivity : AppCompatActivity() {
             val intent: Intent = Intent(this, ConnectYourDate:: class.java)
             startActivity(intent)
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
