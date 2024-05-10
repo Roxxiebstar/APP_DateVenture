a69fec36b505ac959d2a9cd2af08915721fef7e7
Roxxiebstar <kathebuitragor8@gmail.com>
2024-05-10 09:02:40 -0500

    Revert "Actualizando layouts y Constraints"
    This reverts commit 28af186b34af9e01c30266a3fe7c50662170bf16.

--- DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
+++ DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
@@ -27,6 +27,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        binding.appBarMain.fab.setOnClickListener { view ->
+            val intent: Intent = Intent(this, LogIn:: class.java)
+            startActivity(intent)
+            //*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //*    .setAction("Action", null).show()
+        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
