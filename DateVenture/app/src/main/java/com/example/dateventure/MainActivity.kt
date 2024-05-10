57aaf18a0c5a0d7d4df3516cd80726ae789570d6
Sergio Galindo <141683974+Im-Scandal@users.noreply.github.com>
2024-05-10 17:33:57 -0500

    Arreglando detalles v2

--- DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
+++ DateVenture/app/src/main/java/com/example/dateventure/MainActivity.kt
@@ -25,27 +25,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            val intent: Intent = Intent(this, LogIn:: class.java)
-            startActivity(intent)
-            //*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            //*    .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
         val btnPlan: Button = findViewById(R.id.btnPlaneaCita)
         btnPlan.setOnClickListener {
             val intent: Intent = Intent(this, Cuestionario:: class.java)
