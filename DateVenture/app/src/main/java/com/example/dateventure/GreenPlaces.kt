0fbe10b2ccf4844ae824f56077f866108d4ab137
Roxxiebstar <kathebuitragor8@gmail.com>
2024-05-10 18:55:51 -0500

    creando listView para greenPlaces y Travel

--- DateVenture/app/src/main/java/com/example/dateventure/GreenPlaces.kt
+++ DateVenture/app/src/main/java/com/example/dateventure/GreenPlaces.kt
@@ -1,12 +1,16 @@
 package com.example.dateventure
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GreenPlaces : AppCompatActivity() {
+    private val selectedItems = mutableSetOf<Int>()
+    private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,47 @@ class GreenPlaces : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Encuentra el ListView en el diseño
+        val listView: ListView = findViewById(R.id.greenSpacePreferencesListView)
+
+        // Crea una lista de datos
+        val items = mutableListOf("PICNIC EN EL PARQUE", "VISITA A UN JARDÍN BOTÁNICO",
+            "SESIÓN DE DIBUJO AL AIRE LIBRE","PASEO EN BICICLETA POR LA NATURALEZA", "VOLUNTARIADO EN UN HUERTO COMUNITARIO")
+
+        // Crea el ArrayAdapter
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_multiple_choice,
+            items
+        )
+
+        // Enlaza el adaptador con el ListView
+        listView.adapter = adapter
+
+        // Notificar al adaptador que los datos han cambiado
+        adapter.notifyDataSetChanged()
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            if (selectedItems.contains(position)) {
+                selectedItems.remove(position)  // Deseleccionar
+            } else {
+                selectedItems.add(position)  // Seleccionar
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntegerArrayList("selectedItems", ArrayList(selectedItems))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredItems = savedInstanceState.getIntegerArrayList("selectedItems")
+        restoredItems?.forEach { position ->
+            listView.setItemChecked(position, true)  // Restaura la selección
+            selectedItems.add(position)
         }
     }
 }
